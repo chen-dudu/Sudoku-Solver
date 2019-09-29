c448a58e63d08c45d7c7e393e788d0e3ce4d8b2d
unknown <43197141+chen-dudu@users.noreply.github.com>
2019-09-29 15:21:03 +1000

    added base case for valid_row predicate

--- proj2.pl
+++ proj2.pl
@@ -7,14 +7,15 @@
 
 %% check diagonal
 
-%% valid_diagonal(Puzzle) :- length(Puzzle, N), valid_diagonal(Puzzle, X, N).
+%% valid_diagonal([[X|Xs]|Rest]) :- length(Puzzle, N), valid_diagonal(Puzzle, X, N).
 %% valid_diagonal([List|Rest], X, 1) :- .
 %% valid_diagonal([List|Rest], X, N) :- 
 
 
 
 %% chect that all the rows are valid
-valid_row([[X|Xs]|Rest]) :- sum_list(Xs, 0, X), valid_row(Rest).
+valid_row([]).
+valid_row([[X|Xs]|Rest]) :- (sum_list(Xs, 0, X); product_list(Xs, 1, X)), valid_row(Rest).
 
 %% check that all hte columns are valid
 valid_column(Puzzle) :- transpose(Puzzle, Transpose), valid_row(Transpose).
@@ -25,4 +26,13 @@ sum_list([X|Xs], Acc, S) :-
 	Acc1 is X + Acc,
     sum_list(Xs, Acc1, S).
 
+product_list([], P, P).
+product_list([X|Xs] , Acc, P) :-
+    Acc1 is X * Acc,
+    product_list(Xs, Acc1, P).
 
+
+
+%% valid_diagonal([[1, 2], [2, 1]]).
+%% valid_diagonal([[1, 2, 3], [2, 1, 3], [3, 2, 1]).
+%% valid_row([[28, 7, 1, 4]]).
