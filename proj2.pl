804f4c9b9459aa9bc39ca2386f64f56315de99c6
unknown <43197141+chen-dudu@users.noreply.github.com>
2019-10-14 11:01:48 +1100

    changed implementation for asign_value and valid_row by making use of maplist for better code

--- proj2.pl
+++ proj2.pl
@@ -3,19 +3,30 @@
 % Purpose : An implementation of the program that solves math puzzles.
 
 
+%---------------------------------------------------------------------------%
+%---------------------------- Required Libraray ----------------------------%
+%---------------------------------------------------------------------------%
 
-
-
-%% import required library to use transpose/2
+% import required library to use transpose/2
 :- ensure_loaded(library(clpfd)).
 
-puzzle_solution([Heading|Rest]) :- transpose([Heading|Rest], [_|TRest]), 
-                                   valid_digit(Rest), valid_digit(TRest), 
-                                   valid_row(Rest), valid_row(TRest), 
-                                   valid_diagonal([Heading|Rest]), 
-                                   append([Heading|Rest], Vars), 
-                                   asign_value(Vars).
+%---------------------------------------------------------------------------%
+%------------------------------ Main Predicate -----------------------------%
+%---------------------------------------------------------------------------%
+% ddddd
+puzzle_solution([Heading|Rest]) :- 
+    transpose([Heading|Rest], [_|TRest]), 
+    valid_digit(Rest), 
+    valid_digit(TRest), 
+    valid_row(Rest), 
+    valid_row(TRest), 
+    valid_diagonal([Heading|Rest]), 
+    append([Heading|Rest], Vars), 
+    asign_value(Vars).
 
+%---------------------------------------------------------------------------%
+%----------------------------- Helper Predicates ---------------------------%
+%---------------------------------------------------------------------------%
 % valid_digit(?List)
 % List is list of lists, each inner list represents a row in the puzzle
 % This predicate checks that all numbers in the list are between 1 and 9, and
@@ -28,15 +39,18 @@ valid_digit([[_|Cells]|Rest]) :-
 
 
 %% valid_diagonal(-Puzzle)
-valid_diagonal(Puzzle) :- length(Puzzle, Len), index(1, 1, Puzzle, X), 
-                          Padding is Len - 1, 
-                          valid_diagonal(Puzzle, X, Padding).
+valid_diagonal(Puzzle) :- 
+    length(Puzzle, Len), 
+    index(1, 1, Puzzle, X), 
+    Padding is Len - 1, 
+    valid_diagonal(Puzzle, X, Padding).
 
 
 
 %% valid_diagonal(?List, ?X, +N)
 valid_diagonal(Puzzle, X, Padding) :- 
-    length(Puzzle, Len), Index is Len - Padding, 
+    length(Puzzle, Len), 
+    Index is Len - Padding, 
     ( Padding =:= 1 ->
         index(Index, Index, Puzzle, X)
     ;   Padding1 is Padding - 1, 
@@ -53,9 +67,16 @@ index(ColumnNumber, RowNumber, List, Item) :-
 
 
 % valid_row(?List)
-valid_row([]).
-valid_row([[X|Xs]|Rest]) :- 
-    (sum(Xs, #=, X); product_list(Xs, 1, X)), valid_row(Rest).
+%% valid_row([]).
+%% valid_row([[Heading|Row]|Rest]) :- 
+%%     (sum(Row, #=, Heading); product_list(Row, 1, Heading)), valid_row(Rest).
+
+valid_row(Rows) :- maplist(check_valid_row, Rows).
+
+check_valid_row([Heading|Row]) :- 
+    sum(Row, #=, Heading)
+    ;
+    product_list(Row, 1, Heading).
 
 
 % product_list(+List, +Accumulator, -Product)
@@ -74,5 +95,4 @@ product_list([X|Xs] , Acc, P) :-
 % asign_value(?List)
 % This predicate asign values to unbound elements in the List
 % based on each element's own range restriction
-asign_value([]).
-asign_value([X|Xs]) :- indomain(X), asign_value(Xs).
+asign_value(List) :- maplist(indomain, List).
